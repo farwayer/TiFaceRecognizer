6206cf39fd77df142cc53e40b276ab01a5241c7d
farwayer <farwayer@gmail.com>
2013-10-16 11:56:46 +0300

    add some module const

--- Classes/ByFarwayerFacerecognizerDetectorProxy.m
+++ Classes/ByFarwayerFacerecognizerDetectorProxy.m
@@ -1,10 +1,4 @@
-//
-// Created by farwayer on 10/16/13.
-//
-
-
 #import "ByFarwayerFacerecognizerDetectorProxy.h"
-
 
 @implementation ByFarwayerFacerecognizerDetectorProxy {
 
--- Classes/ByFarwayerFacerecognizerModule.h
+++ Classes/ByFarwayerFacerecognizerModule.h
@@ -3,4 +3,16 @@
 @interface ByFarwayerFacerecognizerModule : TiModule {
 }
 
+@property (nonatomic, readonly) NSString *ACCURACY_LOW;
+@property (nonatomic, readonly) NSString *ACCURACY_HIGH;
+
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_TOP_LEFT;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_TOP_RIGHT;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_BOTTOM_RIGHT;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_BOTTOM_LEFT;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_LEFT_TOP;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_RIGHT_TOP;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_RIGHT_BOTTOM;
+@property (nonatomic, readonly) NSNumber *IMAGE_ORIENTATION_LEFT_BOTTOM;
+
 @end
